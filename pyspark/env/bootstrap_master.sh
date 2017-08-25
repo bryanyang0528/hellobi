1ce1aa1c239abfd345e64bb3507d7ad9d80d6203
Bryan Yang <kenshin2004528@gmail.com>
2017-08-25 16:10:14 +0800

    updated

--- pyspark/env/bootstrap_master.sh
+++ pyspark/env/bootstrap_master.sh
@@ -4,5 +4,6 @@ rpm -ivh mysql57-community-release-el7-9.noarch.rpm
 yum install -y mysql-server mysql-connector-java 
 systemctl enable mysqld 
 ln -s /usr/share/java/mysql-connector-java.jar $HIVE_HOME/lib/mysql-connector-java.jar
-wget https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
+pip install numpy pandas ipython[all] jupyter virtualenv
